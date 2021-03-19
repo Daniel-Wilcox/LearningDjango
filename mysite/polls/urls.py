6ef559ab3599af97769ff28533502576df04380d
Daniel <d.dt.wilcox@gmail.com>
2021-03-19 13:26:06 +0200

    completed 3/4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import IndexView, DetailView, ResultsView, vote
 
-from .views import index
-
+app_name = 'polls'
 urlpatterns = [
-    path('', index),
+    path('', IndexView.as_view(), name='index'),
+    path('<int:pk>/', DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
